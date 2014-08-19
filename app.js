676c4f81bafde45a380142e39dd2df2c6aca5aba
Asif Ahmed <asif@parksleepfly.com>
2014-08-18 18:27:43 -0700

    add working code - no promises

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-var myApp = angular.module('instagramSearchApp', []);
+angular.module('instagramSearchApp', [])
+	.controller('InstagramSearchController', function($scope, $http){
+		
+		$scope.getInstagramResults = function(text){
+			var url = 'https://api.instagram.com/v1/tags/'+ $scope.searchTerm +'/media/recent';
+			var request = {
+				callback: 'JSON_CALLBACK',
+				client_id: '2897297205c64c28bb12e13ca1be5332'
+			};
 
-myApp.controller('InstagramSearchController', ['$scope', function($scope){
+			$http({
+				method: 'JSONP',
+				url: url,
+				params: request
+			}).
+			success(function(result){
+				$scope.results = result.data;
+				console.log($scope.results.length);
+			}).
+			error(function(result){
+				console.log("ERROR MAN");
+				console.log(result);
+
+			});
+
+		};
+
 	
 
-}]);
+	});
+
+
+
+// CLIENT ID	2897297205c64c28bb12e13ca1be5332
+// CLIENT SECRET	90f88453be444f21b56fedf3980563bd
+// WEBSITE URL	http://sifxtreme.github.io/thinkful-angular-instagram-searcher/
+// REDIRECT URI	http://sifxtreme.github.io/thinkful-angular-instagram-searcher/
